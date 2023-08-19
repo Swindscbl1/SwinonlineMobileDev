c69ab4011c6f316f1cc1e0ba4b88d44aa8d6bf46
Swindscbl1 <140997665+Swindscbl1@users.noreply.github.com>
2023-08-19 18:57:09 +1000

    Add files via upload

--- app/src/main/java/au/edu/swin/billlo/scoreappp1/MainActivity.kt
+++ app/src/main/java/au/edu/swin/billlo/scoreappp1/MainActivity.kt
@@ -1,82 +1,74 @@
 package au.edu.swin.billlo.scoreappp1
-// MainActivity.kt Bill Lo
+// MainActivity.kt
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
-import android.media.MediaPlayer // Import the MediaPlayer class, Sound from Google's royalty free sound library 2022
-import android.util.Log //logging
+import android.media.MediaPlayer // Import the MediaPlayer class
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var buttonAdd: Button
-    private lateinit var buttonSub: Button
+    private lateinit var buttonRed: Button
     private lateinit var buttonReset: Button
-    private lateinit var textViewScore: TextView
-    private var scoreVal = 0
+    private lateinit var buttonBlue: Button
+    private lateinit var textViewScoreR: TextView
+    private lateinit var textViewScoreB: TextView
+    private var score_r = 0
+    private var score_b = 0
     private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonAdd = findViewById(R.id.buttonAdd)
-        buttonSub = findViewById(R.id.buttonSub)
-        buttonReset = findViewById(R.id.buttonSave)
-        textViewScore = findViewById(R.id.textViewScore)
+        buttonRed = findViewById(R.id.buttonRed)
+        buttonBlue = findViewById(R.id.buttonBlue)
+        buttonReset = findViewById(R.id.buttonReset)
+        textViewScoreR = findViewById(R.id.textViewScoreR)
+        textViewScoreB = findViewById(R.id.textViewScoreB)
 
         // Get the score from the saved instance
         if (savedInstanceState != null) {
-            scoreVal = savedInstanceState.getInt("score")
-            textViewScore.text = getString(R.string.score, scoreVal)// this was working before.
-            Log.i("ScoreApp1", "Loaded Save")
-        }
-        else
-        {
-            Log.i("ScoreApp1", "Failed to load save")
+            score_r = savedInstanceState.getInt("score_r")
+            score_b = savedInstanceState.getInt("score_b")
         }
 
-        textViewScore.text = getString(R.string.score, scoreVal)    // Display the score
+        textViewScoreR.text = getString(R.string.score_r, score_r)    // Display the score
+        textViewScoreB.text = getString(R.string.score_b, score_b)    // Display the score
 
-        // Create a media player object and set to play
-        mediaPlayer = MediaPlayer.create(this, R.raw.bugle_tune)
+        // Create a media player object
+        mediaPlayer = MediaPlayer.create(this, R.raw.beep) // and set to play
 
         // Set the listeners for the buttons
         buttonReset.setOnClickListener {
-            scoreVal = 0
-            textViewScore.text = getString(R.string.score, scoreVal)
-            Log.i("ScoreApp1", "Reset")
+            score_r = 0
+            score_b = 0
+            textViewScoreR.text = getString(R.string.score_r, score_r)
+            textViewScoreB.text = getString(R.string.score_b, score_b)
         }
 
-        buttonAdd.setOnClickListener {
-            // Add to score by 1
-            scoreVal++
-            Log.i("ScoreApp1", "Red score increment")
-            // Update the score on the text view
-            textViewScore.text = getString(R.string.score, scoreVal)
+        buttonRed.setOnClickListener {
+            // Deduct the score by 1
+            score_r++
+            // Display the score on the text view
+            textViewScoreR.text = getString(R.string.score_r, score_r)
             // Check if the score is 15
-            if (scoreVal == 15) {
+            if (score_r == 15) {
                 // Play the sound
                 mediaPlayer.start()
-                Log.i("ScoreApp1", "Red Reached 15, played sound")
-            }
-            if(scoreVal == 16)
-            {
-                scoreVal = 0
-                textViewScore.text = getString(R.string.score, scoreVal)
-                Log.i("ScoreApp1", "Did reset all scores")
             }
         }
 
-        buttonSub.setOnClickListener {
-            // Check if the score is 0
-            if (scoreVal > 0) {
+        buttonBlue.setOnClickListener {
+            // Add the score by 1
+            score_b++
+            // Display the score on the text view
+            textViewScoreB.text = getString(R.string.score_b, score_b)
+            // Check if the score is 15
+            if (score_b == 15) {
                 // Play the sound
-                scoreVal--
-                Log.i("ScoreApp1", "score dec")
-                // Display the score on the text view
-                textViewScore.text = getString(R.string.score, scoreVal)
-                Log.i("ScoreApp1", "Blue Reached 15, played sound")
+                mediaPlayer.start()
             }
         }
     }
@@ -84,12 +76,8 @@ class MainActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         // Save the score in the bundle with the key "score"
-        outState.putInt("score", scoreVal)
-        Log.i("ScoreApp1", "Save enacted")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mediaPlayer.release()
+        outState.putInt("score_r", score_r)
+        outState.putInt("score_b", score_b)
     }
 }
+
